4cefaca941fa144e35eee50a879d7abced94c115
Alex Hollis <mr.alex.hollis@gmail.com>
2020-10-08 22:53:45 +0100

    Updated index.html with content

--- assets/js/core.js
+++ assets/js/core.js
@@ -266,7 +266,7 @@ var Mi = {
 
                 var pickedStyle = $googleMap.data('style');   
                 var myOptions = {
-                    zoom: 6,
+                    zoom: 5,
                     center: new google.maps.LatLng(yourLatitude,yourLongitude-0.03),
                     mapTypeId: google.maps.MapTypeId.ROADMAP,
                     mapTypeControl: false,
